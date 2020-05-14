7ff83e625003c7f029677a8afae39d142dcc87da
Lukasz <lukaszkkoziel@gmail.com>
2020-05-14 21:06:54 +0900

    all options working

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,41 +1,27 @@
 class CocktailsController < ApplicationController
-
-  def new
-    @cocktail = Cocktail.new
-  end
-
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.new
     @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
   end
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to @cocktail, notice: 'Cocktail created!.'
+      redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      render 'new'
     end
   end
 
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def destroy
-    # @Cocktail.destroy
-    # redirect_to Cocktails_url, notice: 'Cocktail destroyed!.'
-  end
-
-private
+  private
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,8 +1,8 @@
 class DosesController < ApplicationController
-  def new
-    @dose = Dose.new
-    @cocktail = Cocktail.find(params[:cocktail_id])
-  end
+   def new
+     @cocktail = Cocktail.find(params[:cocktail_id])
+     @dose = Dose.new
+   end
 
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
@@ -11,15 +11,18 @@ class DosesController < ApplicationController
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :new
+      @review = Review.new
+      render "cocktails/show"
     end
   end
 
   def destroy
-    @dose = Dose.find(perams[:id])
+    @dose = Dose.find(params[:id])
     @dose.destroy
     redirect_to cocktail_path(@dose.cocktail)
   end
+
+  private
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
