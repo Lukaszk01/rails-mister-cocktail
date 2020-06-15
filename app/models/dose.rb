fd0e245a84e332cd498bb5111158bf781fb2a608
Lukasz <lukaszkkoziel@gmail.com>
2020-06-15 17:52:18 +0900

    validation changed

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,7 +2,7 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
-  validates :ingredient, presence: true, uniqueness: true
+  # validates :ingredient, presence: true, uniqueness: true
   validates :description, presence: true
   # validates  uniqueness: { scope: :description }
 end
